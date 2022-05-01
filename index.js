5ff9ac32160f65b05c566ff6fdd9d714bd69e2db
HabiburRahmanAnik <habib264521@gmail.com>
2022-05-01 23:27:33 +0600

    implement find inventory with help of email

--- index.js
+++ index.js
@@ -100,6 +100,15 @@ async function run() {
       const result = await inventoryCollection.deleteOne(query);
       res.send(result);
     });
+
+    // FIND ITEM BY EMAIL
+    app.get('/myItem', async (req, res) => {
+      const email = req.query.email;
+      const query = { email };
+      const cursor = inventoryCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // await client.close()
   }
